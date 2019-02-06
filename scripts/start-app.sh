2f14454688d26a676d81afd56566dd70f18712a7
root <root@ip-172-31-83-195.ec2.internal>
2019-02-06 03:52:56 +0000

    added bin/bash to top of script

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,5 +1,7 @@
+#!/bin/sh
 export PATH=/home/ec2-user/apache-maven-3.6.0/bin:$PATH
 export PATH=/home/ec2-user/node-v11.9.0-linux-x64/bin:$PATH
+echo $PATH
 sudo service mongod start
 cd /home/ec2-user/generic-backend
 mvn spring-boot:run &
